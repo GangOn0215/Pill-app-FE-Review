954b3aecd6f24ae94e887f4dca02f009c0a84498
GangOn0215 <ekdldktnekf@gmail.com>
2022-03-29 23:18:24 +0900

    Set Initialize React

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+// React - useState, useRef, useEffect, useMemo, React.memo, useCallback, useReducer, useContext
+// Router - useRouter, Link
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h2>Welcome to 1 day 3 pill</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
